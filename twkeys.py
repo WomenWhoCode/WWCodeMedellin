1fc019889efafcb7f7f9d9da61683a3ee465d045
Isabel Yepes <iyepes0120@gmail.com>
2018-08-10 12:21:36 -0500

    Create twkeys.py
    Upload code

---
+++ twkeys.py
@@ -0,0 +1,16 @@
+#File to isolate and reuse Twitter API Credentials
+def consumer_key():
+	#API Key
+	return "Add Consumer Key here"
+
+def consumer_secret():
+	#API Secret
+	return "Add Consumer Secret here"
+
+def access_key():
+	#Access Key
+	return "Add Access Token here"
+
+def access_secret():
+	#Access Secret
+	return "Add Access Token Secret here"
